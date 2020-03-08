00e25fd76749eb0ff55ad11292540ee7c73f6d0e
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2020-03-09 00:07:56 +0300

    add query scope betweendates

--- app/Bookable.php
+++ app/Bookable.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bookable extends Model
 {
-	public function booking()
+	public function bookings()
 	{
 		return $this->hasMany(Booking::class);
 	}
--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Bookable;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -13,13 +14,15 @@ class BookableAvailabilityController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke($id, Request $request)
     {
 		$data = $request->validate([
             'from' => 'required|date_format:Y-m-d|after_or_equal:now',
             'to' => 'required|date_format:Y-m-d|after_or_equal:from'
 		]);
 		
-		dd($data);
+		$bookable = Bookable::findOrFail($id);
+
+		dd($bookable->bookings()->betweenDates($data['from'], $data['to'])->count());
     }
 }
