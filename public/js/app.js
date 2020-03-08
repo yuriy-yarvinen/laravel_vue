d3b2d26de4c030d5d69c1afa3ec93fc3ec60f485
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2020-03-09 01:03:58 +0300

    displaying-availability

--- public/js/app.js
+++ public/js/app.js
@@ -1946,6 +1946,10 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   //v-on = @
   data: function data() {
@@ -1989,7 +1993,7 @@ __webpack_require__.r(__webpack_exports__);
       return 200 === this.status;
     },
     noAvailability: function noAvailability() {
-      return 400 === this.status;
+      return 404 === this.status;
     }
   }
 });
@@ -38228,7 +38232,18 @@ var render = function() {
     _c(
       "h6",
       { staticClass: "text-uppercase text-secondary font-weight-bolder" },
-      [_vm._v("Check Availability")]
+      [
+        _vm._v("\n      Check Availability\n      "),
+        _vm.noAvailability
+          ? _c("span", { staticClass: "text-danger" }, [
+              _vm._v("(NOT AVAILABLE)")
+            ])
+          : _vm._e(),
+        _vm._v(" "),
+        _vm.hasAvailability
+          ? _c("span", { staticClass: "text-success" }, [_vm._v("(AVAILABLE)")])
+          : _vm._e()
+      ]
     ),
     _vm._v(" "),
     _c("div", { staticClass: "form-row" }, [
