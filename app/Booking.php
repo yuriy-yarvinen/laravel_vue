24e763ed821c1d1544da83c6c323008be69f02c1
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2020-03-09 00:19:15 +0300

    rebuild scope to make easier

--- app/Booking.php
+++ app/Booking.php
@@ -11,10 +11,11 @@ class Booking extends Model
 	public function bookable()
 	{
 		return $this->belongsTo(Bookable::class);
-	}
+	}	
 
 	public function scopeBetweenDates(Builder $query, $from, $to)
 	{
 		return $query->where('to', '>=', $from)->where('from', '<=', $to);
 	}
+
 }
