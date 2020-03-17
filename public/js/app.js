394faa5ed1e8ec5f5243c5a344e872858d3b73ae
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2020-03-17 23:49:14 +0300

    rating-flow

--- public/js/app.js
+++ public/js/app.js
@@ -2264,7 +2264,11 @@ __webpack_require__.r(__webpack_exports__);
         content: null
       }
     };
-  } // methods: {
+  },
+  created: function created() {} // 1. If review already exists (in reviews table by id)
+  // 2. Fetch a booking by a review key
+  // 3. Store the review
+  // methods: {
   //   onRatingChanged(rating) {
   //     console.log(rating);
   //   }
@@ -56397,30 +56401,40 @@ var render = function() {
       1
     ),
     _vm._v(" "),
-    _vm._m(0),
+    _c("div", { staticClass: "form-group" }, [
+      _c("label", { staticClass: "text-muted", attrs: { for: "content" } }, [
+        _vm._v("Describe your expirience with")
+      ]),
+      _vm._v(" "),
+      _c("textarea", {
+        directives: [
+          {
+            name: "model",
+            rawName: "v-model",
+            value: _vm.review.content,
+            expression: "review.content"
+          }
+        ],
+        staticClass: "form-control",
+        attrs: { name: "content", cols: "30", rows: "10" },
+        domProps: { value: _vm.review.content },
+        on: {
+          input: function($event) {
+            if ($event.target.composing) {
+              return
+            }
+            _vm.$set(_vm.review, "content", $event.target.value)
+          }
+        }
+      })
+    ]),
     _vm._v(" "),
     _c("button", { staticClass: "btn btn-lg btn-primary btn-block" }, [
       _vm._v("Submit")
     ])
   ])
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "form-group" }, [
-      _c("label", { staticClass: "text-muted", attrs: { for: "content" } }, [
-        _vm._v("Describe your expirience with")
-      ]),
-      _vm._v(" "),
-      _c("textarea", {
-        staticClass: "form-control",
-        attrs: { name: "content", cols: "30", rows: "10" }
-      })
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
