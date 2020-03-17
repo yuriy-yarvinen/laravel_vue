ff857c92d9acfae5bf7de75e7596afadfd318d5f
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2020-03-17 23:20:00 +0300

    add review component

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,7 @@
 import VueRouter from "vue-router";
 import Bookables from "./bookables/Bookables.vue";
 import Bookable from "./bookable/Bookable.vue";
+import Review from "./review/Review.vue";
 
 const routes = [
 	{
@@ -12,6 +13,11 @@ const routes = [
 		path: '/bookables/:id',
 		component: Bookable,
 		name: 'bookable',
+	},
+	{
+		path: '/review/:id',
+		component: Review,
+		name: 'review',
 	},
 
 ];
