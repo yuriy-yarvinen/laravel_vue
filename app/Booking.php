918c004c6c7a711606264f15cdc7cc9d64a514d1
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2020-03-19 00:11:18 +0300

    0071-booking-review-key-model-events

--- app/Booking.php
+++ app/Booking.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Booking extends Model
 {
@@ -22,5 +23,16 @@ class Booking extends Model
 	public function review()
     {
         return $this->hasOne(Review::class);
+	}
+	
+
+	protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($booking) {
+            $booking->review_key = Str::uuid();
+        });
     }
+
 }
