73067d07973124996e7fcbb1704392a6d0498d19
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2020-04-11 23:06:52 +0300

    #0116-refactoring-price-calculations

--- app/Http/Controllers/Api/BookablePriceController.php
+++ app/Http/Controllers/Api/BookablePriceController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Bookable;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
 
 class BookablePriceController extends Controller
 {
@@ -24,16 +23,8 @@ class BookablePriceController extends Controller
             'to' => 'required|date_format:Y-m-d|after_or_equal:from'
         ]);
 
-        $days = (new Carbon($data['from']))->diffInDays(new Carbon($data['to'])) + 1;
-        $price = $days * $bookable->price;
-
         return response()->json([
-            'data' => [
-                'total' => $price,
-                'breakdown' => [
-                    $bookable->price => $days
-                ]
-            ]
+            'data' => $bookable->priceFor($data['from'], $data['to'])
         ]);
     }
 }
